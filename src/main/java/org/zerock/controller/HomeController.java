96d2edf23ec99b58be111c94640579be0714d571
gaeunkim26 <ggim57727@gmail.com>
2025-04-19 01:45:33 +0900

    fork후commit test

--- src/main/java/org/zerock/controller/HomeController.java
+++ src/main/java/org/zerock/controller/HomeController.java
@@ -20,6 +20,8 @@ public class HomeController {
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
 		logger.info("Welcome home! The client locale is {}.", locale);
+
+		//250419 fork후 commit test
 		
 		return "home";
 	}
